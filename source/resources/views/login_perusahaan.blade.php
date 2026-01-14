84a5bb32d4fd5275e07c5bf127db30d51484a4b5
Meteor95 <mochamad.aries.styawan@gmail.com>
2026-01-14 10:51:27 +0700

    test

--- source/resources/views/login_perusahaan.blade.php
+++ source/resources/views/login_perusahaan.blade.php
@@ -1,70 +1,48 @@
 @extends('templatebody')
 @section('konten_utama')
-<div class="login-background">
-  <div class="login-center">
-    <div class="login-card login-dark">
-      <div class="text-center mb-3">
-        <a href="{{ url('') }}" class="logo">
-          <img src="{{ asset('mofi/assets/images/logo/Logo_AMC_Full.png') }}" alt="Logo AMC" class="img-fluid" style="max-width: 250px;">
-        </a>
-      </div>
-      <div class="theme-form">
-        <h4 class="text-center mb-2">Masukkan Informasi Yang Diberikan TIM AMC</h4>
-        <p class="text-center mb-4">Ketikan Surel / Nama Pengguna dan Katasandi Terdaftar</p>
-        <div class="form-group">
-          <label>ID Perusahaan</label>
-          <input autocomplete="off" value="erayadigitalstudio" class="form-control" type="text" id="namapengguna" placeholder="hallo@arthamedicalcenter.com">
-        </div>
-        <div class="form-group">
-          <label>Katasandi</label>
-          <div class="form-input position-relative">
-            <input autocomplete="off" value="Salam1jiwa" class="form-control" type="password" id="katasandi" placeholder="*********">
-            <div class="show-hide"><span class="show"></span></div>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-xl-5 p-0">
+      <div class="login-card login-dark">
+        <div>
+          <div class="d-flex justify-content-center">
+            <a class="logo text-center" href="{{url('')}}">
+                <img class="img-fluid mx-auto d-block" src="{{asset('mofi/assets/images/logo/Logo_AMC_Full.png')}}" alt="looginpage">
+            </a>
+          </div>
+          <div class="login-main"> 
+            <div class="theme-form">
+              <h4>Masukan Informasi Yang Diberikan TIM AMC</h4>
+              <p>Ketikan Surel / Nama Pengguna dan Katasandi Terdaftar</p>
+              <div class="form-group">
+                <label class="col-form-label">ID Perusahaan</label>
+                <input autocomplete="off" value="erayadigitalstudio" class="form-control" type="text" id="namapengguna" required="" placeholder="hallo@arthamedicalcenter.com">
+              </div>
+              <div class="form-group">
+                <label class="col-form-label">Katasandi</label>
+                <div class="form-input position-relative">
+                  <input autocomplete="off" value="Salam1jiwa" class="form-control" name="password" type="password" id="katasandi" required="" placeholder="*********">
+                  <div class="show-hide"><span class="show"></span></div>
+                </div>
+              </div>
+              <div class="form-group mb-0">
+                <button class="btn btn-primary btn-block w-100" id="btn_login">Kirim Data</button>
+              </div>
+              <p class="mt-4 mb-0 text-center">Versi Aplikasi : {{env('APP_VERSION')}}</p>
+              <p class="mb-0 text-center" id="visitor_id" class="text-muted"></p>
+            </div>
           </div>
         </div>
-        <div class="form-group mb-3">
-          <button class="btn btn-primary btn-block w-100" id="btn_login">Kirim Data</button>
-        </div>
-        <p class="text-center mb-0">Versi Aplikasi : {{ env('APP_VERSION') }}</p>
-        <p class="text-center text-muted" id="visitor_id"></p>
       </div>
     </div>
+    <div class="col-xl-7"><img class="bg-img-cover" style="background-position: right center;" src="{{asset('mofi/assets/images/logo/banner_perusahaan.jpg')}}" alt="looginpage"></div>
   </div>
 </div>
 @endsection
-
 @section('css_load')
-<style>
-/* Fullscreen background */
-.login-background {
-    background: url('{{ asset("mofi/assets/images/logo/banner_perusahaan.jpg") }}') no-repeat center center;
-    background-size: cover;
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-
-/* Center login card */
-.login-center {
-    width: 100%;
-    max-width: 400px; /* ukuran card */
-    padding: 15px;
-}
-
-/* Optional: login card styling */
-.login-card {
-    background: rgba(0,0,0,0.65); /* semi-transparent background */
-    padding: 30px;
-    border-radius: 12px;
-    color: #fff;
-}
-</style>
 @endsection
-
 @section('js_load')
 <script src="https://cdn.jsdelivr.net/npm/@fingerprintjs/fingerprintjs@4.5.1/dist/fp.min.js"></script>
-<script src="{{ asset('mofi/assets/js/script.js') }}"></script>
+<script src="{{asset('mofi/assets/js/script.js')}}"></script>
 <script src="{{ asset('mofi/assets/js/system/login.js') }}?v={{ filemtime(public_path('mofi/assets/js/system/login.js')) }}"></script>
 @endsection
