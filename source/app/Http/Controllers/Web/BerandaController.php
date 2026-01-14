2b939b4bb3385a23cd07d1668f3716cfbf7619e5
Meteor95 <mochamad.aries.styawan@gmail.com>
2026-01-14 13:43:25 +0700

    Dashboard perusahaan

--- source/app/Http/Controllers/Web/BerandaController.php
+++ source/app/Http/Controllers/Web/BerandaController.php
@@ -18,9 +18,14 @@ class BerandaController extends Controller
             'user_details' => $req->attributes->get('user_details'),
         ];
     }
-    public function index(Request $request)
-    {
-        $data = $this->getData($request, 'Beranda MCU Arta Medica Centre', [
+    public function index(Request $request){
+        $user = $request->attributes->get('user_details');
+        $perusahaan = json_decode($user->json_perusahaan, true);
+        $judul = 'Beranda Aplikasi Arta Medica Centre';
+        if (!empty($perusahaan)) {
+            $judul = 'Beranda Partner Artha Medica Centre';
+        }
+        $data = $this->getData($request, $judul , [
             'Dashboard' => route('admin.beranda'),
         ]);
         $data['jumlah_member_terdaftar'] = MemberMCU::count();
@@ -28,18 +33,19 @@ class BerandaController extends Controller
         $data['jumlah_rekanan'] = Perusahaan::count();
         $data['jumlah_tindakan_selesai'] = Transaksi::where('status_peserta', 'selesai')->count();
         $data['jumlah_tindakan_proses'] = Transaksi::where('status_peserta', 'proses')->count();
+        if (!empty($perusahaan)) {
+            return view('paneladmin.beranda.main_konten_perusahaan', ['data' => $data]);    
+        }
         return view('paneladmin.beranda.main_konten', ['data' => $data]);
     }
-    public function kasir(Request $request)
-    {
+    public function kasir(Request $request){
         $data = $this->getData($request, 'Kasir MCU Arta Medica Centre', [
             'Kasir' => route('admin.kasir'),
         ]);
         $data['bank'] = DaftarBank::all();
         return view('paneladmin.beranda.kasir', ['data' => $data]);
     }
-    public function antrian(Request $request)
-    {
+    public function antrian(Request $request){
         $data = $this->getData($request, 'Antrian Pasien Di Ruangan', [
             'Antrian Pasien' => route('admin.antrian'),
         ]);
---
+++ source/resources/views/paneladmin/beranda/main_konten_perusahaan.blade.php
@@ -0,0 +1,107 @@
+@extends('paneladmin.templateadmin')
+@section('konten_utama_admin')
+<div class="row">
+  <div class="col-md-12">
+    <div class="card common-hover">
+      <div class="card-header border-l-primary border-3">
+        <h4>Informasi Dasar Aplikasi</h4>
+        <p>Bagian ini menampilkan ringkasan data utama yang menggambarkan aktivitas dan penggunaan aplikasi secara keseluruhan. Informasi yang ditampilkan meliputi jumlah member yang terdaftar, total pasien terdaftar, jumlah pasien yang melakukan pendaftaran secara online, serta statistik transaksi yang sedang dalam proses maupun transaksi yang telah berhasil diselesaikan.</p>
+      </div>
+      <div class="card-body">
+        <div class="row">
+          <div class="col-sm-4">
+            <div class="card">
+              <div class="card-body student">
+                <div class="d-flex gap-2 align-items-end"> 
+                  <div class="flex-grow-1">
+                    <h2><span id="dashboard_total_berkas">{{ $data['jumlah_member_terdaftar'] }}</span> Orang</h2>
+                    <p class="mb-0 text-truncate"> Member Terdaftar Di AMC</p>
+                    <div class="d-flex student-arrow text-truncate">
+                      
+                    </div>
+                  </div>
+                  <div class="flex-shrink-0"><img src="https://admin.pixelstrap.net/mofi/assets/images/dashboard-4/icon/student.png" alt=""></div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-sm-4">
+            <div class="card">
+              <div class="card-body student">
+                <div class="d-flex gap-2 align-items-end"> 
+                  <div class="flex-grow-1">
+                    <h2><span id="dashboard_total_berkas">{{ $data['jumlah_transaksi'] }}</span> Trx</h2>
+                    <p class="mb-0 text-truncate"> Transaksi Peserta Di AMC</p>
+                    <div class="d-flex student-arrow text-truncate">
+                      
+                    </div>
+                  </div>
+                  <div class="flex-shrink-0"><img src="https://admin.pixelstrap.net/mofi/assets/images/dashboard-4/icon/student.png" alt=""></div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-sm-4">
+            <div class="card">
+              <div class="card-body student">
+                <div class="d-flex gap-2 align-items-end"> 
+                  <div class="flex-grow-1">
+                    <h2><span id="dashboard_total_berkas">{{ $data['jumlah_rekanan'] }}</span> Perusahaan</h2>
+                    <p class="mb-0 text-truncate"> Rekanan Perusahaan AMC</p>
+                    <div class="d-flex student-arrow text-truncate">
+                      
+                    </div>
+                  </div>
+                  <div class="flex-shrink-0"><img src="https://admin.pixelstrap.net/mofi/assets/images/dashboard-4/icon/student.png" alt=""></div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-sm-6">
+            <div class="card">
+              <div class="card-body student">
+                <div class="d-flex gap-2 align-items-end"> 
+                  <div class="flex-grow-1">
+                    <h2><span id="dashboard_total_berkas">{{ $data['jumlah_tindakan_selesai'] }}</span> Berkas</h2>
+                    <p class="mb-0 text-truncate"> Transaksi Tindakan Selesai</p>
+                    <div class="d-flex student-arrow text-truncate">
+                      
+                    </div>
+                  </div>
+                  <div class="flex-shrink-0"><img src="https://admin.pixelstrap.net/mofi/assets/images/dashboard-4/icon/student.png" alt=""></div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-sm-6">
+            <div class="card">
+              <div class="card-body student">
+                <div class="d-flex gap-2 align-items-end"> 
+                  <div class="flex-grow-1">
+                    <h2><span id="dashboard_total_berkas">{{ $data['jumlah_tindakan_proses'] }}</span> Berkas</h2>
+                    <p class="mb-0 text-truncate"> Transaksi Tindakan Progress</p>
+                    <div class="d-flex student-arrow text-truncate">
+                      
+                    </div>
+                  </div>
+                  <div class="flex-shrink-0"><img src="https://admin.pixelstrap.net/mofi/assets/images/dashboard-4/icon/student.png" alt=""></div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
+@section('css_load')
+@endsection
+@section('js_load')
+<script src="{{ asset('vendor/erayadigital/beranda/beranda.js') }}?v={{ filemtime(public_path('vendor/erayadigital/beranda/beranda.js')) }}"></script>
+@endsection
